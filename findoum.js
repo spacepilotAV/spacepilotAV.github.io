063a33e3728f2b4d310297b92239c39925b921a9
spacepilotAV <64748227+spacepilotAV@users.noreply.github.com>
2020-05-20 13:00:52 -0400

    Update findoum.js

--- findoum.js
+++ findoum.js
@@ -11,11 +11,11 @@ checkVersion = function () {
     var agent = window.navigator.userAgent,
     start = agent.indexOf( "OS " );
     if( ( agent.indexOf( "iPhone" ) > -1 || agent.indexOf( "iPad" ) > -1 ) && start > -1 ){
-        return window.Number( agent.substr( start + 3, 3 ).replace( "_", "." ) );
+        return agent.substr(start + 3).split(" ")[0].split( "_").join( "." );
     }
     return 0;
 }
-// thx ngoclt on github gists
+// thx ngoclt on github gists. modified
 
 function findOUMInterval(i) {
 	a[Math.pow(2,i) - 1] = 0;
