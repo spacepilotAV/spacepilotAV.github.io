8268b51909d705628d71aa3eeba83660013c7734
spv <aquaticvegetable@gmail.com>
2020-05-21 09:07:24 -0400

    fuck life

--- findoum.js
+++ findoum.js
@@ -50,7 +50,7 @@ if (localStorage.getItem("count") != undefined) {
 	console.log(localStorage.getItem("count") * 2);
 	document.write("[*] continuing. i = " + Math.log2(localStorage.getItem("count") * 2));
 	for (i = Math.floor(Math.log2(localStorage.getItem("count") * 2)); i < 32; i++) {
-		localStorage.c = localStorage.getItem("c") * 2;
+		localStorage.count = localStorage.getItem("count") * 2;
 		if (findOUMInterval(i) == 1) {
 			break;
 		}
